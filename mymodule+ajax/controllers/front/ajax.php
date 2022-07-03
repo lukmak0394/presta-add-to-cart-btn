00a5b97cf31e6ea616481a18c7085734b07689a0
lukmak0394 <lukmak0394@wp.pl>
2022-07-03 20:48:39 +0200

    created ajax controller for my module

---
+++ mymodule+ajax/controllers/front/ajax.php
@@ -0,0 +1,24 @@
+<?php
+
+class MyModuleAjaxModuleFrontController extends ModuleFrontController
+{
+    public function initContent()
+    {
+
+        $context = Context::getContext();
+        $cart = $context->cart;
+        $product_id = $_POST['product_id'];
+        $contains_product = $cart->containsProduct($product_id);
+
+        // Passed product id and info if cart contains product with specific id to array
+        $response = array('status' => true, 'product_id' => $_POST['product_id'], 'containsproduct' => $contains_product);
+
+        // 
+        $json = json_encode($response);
+        echo $json;
+        die;
+
+    }
+}
+
+?>
---
+++ mymodule+ajax/views/templates/front/productExists.tpl
@@ -0,0 +1,5 @@
+<div class="inactive-btn-wrapper">
+    <button class="btn add-to-cart custom-btn disabled" type="submit">
+        {l s='Add to cart' d='Shop.Theme.Actions'}
+    </button>
+</div>
